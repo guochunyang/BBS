14d36650dd1d780560f9b0c81ad42e4276c70331
Chunyang Guo <guochy2012@gmail.com>
2014-01-09 17:55:49 +0800

    实现基本的登陆功能

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+
+  def sign_in(user)
+    session[:user_id] = user.id
+    self.current_user = user
+  end
+
+
+  def sign_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
 end
